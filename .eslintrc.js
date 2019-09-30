697533e89bcace92333658ed99b379cf577cd699
Nienke Dekker <nienke.dekker@nos.nl>
2019-09-30 22:34:24 +0200

    fix indenting

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,31 +1,31 @@
 module.exports = {
-	root: true,
-	env: {
-		node: true,
-		browser: true,
-		es6: true,
-	},
-	extends: [
-		'airbnb-base',
-		'plugin:vue/recommended',
-	],
-	rules: {
-		'import/no-unresolved': 0,
-		'import/prefer-default-export': 0,
-		'vue/html-self-closing': 'off',
-		'vue/no-v-html': 'off',
-		'vue/html-indent': ['error', 2, {
-			'attribute': 1,
-			'closeBracket': 0,
-			'alignAttributesVertically': true,
-		}],
-		'no-cond-assign': 'off',
-		'indent': 'off',
-		'max-len': 0,
-		'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off', // Allow console in development but not on production
-		'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off', // Allow debugger in development but not on production
-	},
-	parserOptions: {
-		parser: 'babel-eslint'
-	}
+  root: true,
+  env: {
+    node: true,
+    browser: true,
+    es6: true,
+  },
+  extends: [
+    'airbnb-base',
+    'plugin:vue/recommended',
+  ],
+  rules: {
+    'import/no-unresolved': 0,
+    'import/prefer-default-export': 0,
+    'vue/html-self-closing': 'off',
+    'vue/no-v-html': 'off',
+    'vue/html-indent': ['error', 2, {
+      'attribute': 1,
+      'closeBracket': 0,
+      'alignAttributesVertically': true,
+    }],
+    'no-cond-assign': 'off',
+    'indent': 'off',
+    'max-len': 0,
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off', // Allow console in development but not on production
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off', // Allow debugger in development but not on production
+  },
+  parserOptions: {
+    parser: 'babel-eslint'
+  }
 };
